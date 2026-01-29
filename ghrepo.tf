ea7a4425f14bf32436a0b0947be464345b2f9c51
Bill Grant <39649812+billgrant@users.noreply.github.com>
2026-01-29 18:14:52 -0500

    feat: add GitHub Actions secrets for TFC token

--- ghrepo.tf
+++ ghrepo.tf
@@ -19,3 +19,15 @@ resource "github_repository" "production" {
     include_all_branches = true
   }
 }
+
+resource "github_actions_secret" "tfc_token_dev" {
+  repository      = github_repository.development.name
+  secret_name     = "TFC_TOKEN"
+  plaintext_value = var.tfc_token
+}
+
+resource "github_actions_secret" "tfc_token_prod" {
+  repository      = github_repository.production.name
+  secret_name     = "TFC_TOKEN"
+  plaintext_value = var.tfc_token
+}
